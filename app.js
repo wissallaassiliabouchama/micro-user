c101796ce7f3fc961423a513e37726fd41812f20
Muhammad Ahsan Khan <muhammadahsankhana@Muhammads-MacBook-Pro-2.local>
2020-06-30 13:36:51 +0500

    “micro-user”

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require("express")
+const path = require('path');
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get("/home", function(req,res){
+    console.log("I am home");
+    res.send("Hello everyone");
+
+});
+
+app.get("/", function(req,res){
+    res.render("user_login");
+    console.log("User login");
+
+
+app.get("/admin_login", function(req,res){
+    res.render("admin_login");
+    console.log("User login");
+
+});
+
+app.get("/user_login", function(req,res){
+    res.render("user_login");
+    console.log("User login");
+
+});
+
+app.get("/user_in", function(req,res){ 
+    var Name = req.query.name;
+    var Password = req.query.pass;
+
+if (Password =="123")
+{
+ 
+  console.log("Successfully logged in as Customer");
+  res.render("users");
+}
+else{
+    res.render("notfound.ejs");
+}
+});
+module.exports.app = app;
+    
+// use this code section if you want to pass static port
+/*app.listen(3000 , function(){
+    console.log("App is running");
+});*/
---
+++ readme.md
@@ -0,0 +1,16 @@
+## This is a microserivce for user
+
+1. Open your terminal and run command `git clone https://github.com/mahsankhaan/micro-user.git`.   
+2. Now if you want to run this project locally, kindly **uncomment** the bottom section from app.js file (open the file and check more details how to uncomment the section )
+3. Now from your terminal change your directory to the folder where you downloaded this project.
+4. Once you're in the project directory ,now run command `node app.js`.
+5. And now you can see the Login page. 
+
+![GitHub Logo](image/login.png)
+
+### NOTE: In login screen, password is "123" and username is "user"
+
+6. Once successfully login, you can see the admin dashboard.
+
+![GitHub Logo](image/dashboard.png)
+
